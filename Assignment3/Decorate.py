fd4397bdfda5ae0fd6c60bfd9054658e9be7c579
INBAL-PC\USER <inbalros@post.bgu.ac.il>
2019-01-06 16:06:17 +0200

    changes in random and sin

--- Assignment3/Decorate.py
+++ Assignment3/Decorate.py
@@ -25,15 +25,35 @@ def predict_ensamble(ensamble, data):
     prediction = 0
     for model in ensamble:
         prediction += model.predict_proba(data)
-    return prediction / len(ensamble)
+
+    prediction /= len(ensamble)
+
+    index_prd = np.argmax(prediction,axis=1)
+    the_prd = [(ensamble[0].classes_)[index] for index in index_prd]
+    return the_prd
 
 
-def inverse_presictions(labels):
+def inverse_presictions(labels,original_lables):
+    original_unique, original_counts = np.unique(original_lables, return_counts=True)
     unique, counts = np.unique(labels, return_counts=True)
-    counts = counts/len(labels)
+    laplas = abs(len(original_unique) - len(unique))
+
+    #counts = np.append(counts,[1 for i in range(laplas)])
+
+    innerIndex = 0
+    for index in range(len(original_unique)):
+        if innerIndex>=len(unique):
+            counts = np.insert(counts,index,1)
+        elif (original_unique[index] != unique[innerIndex]):
+            counts = np.insert(counts,index,1)
+        else:
+            innerIndex+=1
+
+
+    counts = counts/(len(labels)+laplas)
     machane = sum(1/counts)
     counts = ((1/counts)/machane)
-    return (pd.Series(choices(unique,counts,k=len(labels)))).values
+    return (pd.Series(choices(original_unique,counts,k=len(labels)))).values
 
 
 def generate_gant_data(R_size, data ,x_names,y_names,ensamble):
@@ -56,7 +76,7 @@ def generate_random_data(R_size, data ,x_names,y_names,ensamble):
         new_data = new_data.append(pd.Series(newRow,index = x_names),ignore_index=True)
 
     pred = predict_ensamble(ensamble, new_data)
-    new_data[y_names] = (inverse_presictions(pred))
+    new_data[y_names] = (inverse_presictions(pred,data[y_names]))
     data = data.append(new_data,ignore_index=True)
     return data
 
@@ -70,7 +90,7 @@ def encode_categorial(data):
             data[col] = data[col].astype('category')
 
 
-def Decorate(base_learn, data,x_names,y_names,gant_flag=False, ensamble_max_size=3, max_iteration=10, R_size=0.1):
+def Decorate(base_learn, data,x_names,y_names,gant_flag=False, ensamble_max_size=5, max_iteration=10, R_size=0.1):
     original_data = data.copy()
     # original_lables = lables.copy()
     ensamble_size = 1
@@ -81,13 +101,13 @@ def Decorate(base_learn, data,x_names,y_names,gant_flag=False, ensamble_max_size
     ensamble_error = 1 - model.score(original_data[x_names],original_data[y_names])
     while ensamble_size < ensamble_max_size and trails < max_iteration:
         if gant_flag:
-            data = generate_gant_data(R_size, data ,x_names,y_names,ensamble)
+            data = generate_gant_data(R_size, original_data ,x_names,y_names,ensamble)
         else:
-            data = generate_random_data(R_size, data ,x_names,y_names,ensamble)
+            data = generate_random_data(R_size, original_data ,x_names,y_names,ensamble)
         model = base_learn.fit(data[x_names],data[y_names])
         ensamble.append(model)
         ensamble_new_error = calc_ensamble_error(ensamble, original_data[x_names],original_data[y_names])
-        if ensamble_new_error < ensamble_error:
+        if ensamble_new_error <= ensamble_error:
             ensamble_size += 1
             ensamble_error= ensamble_new_error
         else:
@@ -105,5 +125,5 @@ encode_categorial(cars)
 X_names =  ["buying", "maint", "doors", "persons", "lug_boot", "safety"]
 y_names = "acceptability"
 
-Decorate(DecisionTreeClassifier(),cars,X_names,y_names)
+Decorate(DecisionTreeClassifier(max_depth=5),cars,X_names,y_names)
 
